0a9be319126e12cbe2c52084ccec65d51b340426
Dioguinho Matador <mtd5000@gmail.com>
2023-08-24 13:56:00 +0100

    unset env

--- builtins3.c
+++ builtins3.c
@@ -12,6 +12,37 @@
 
 #include "minishell.h"
 
+void ft_del_env(t_list **head, char *name)
+{
+  t_list  *current;
+  t_env *exp_node;
+  t_list  *previous;
+  t_env *new_node;
+
+  current = *head;
+  previous = NULL;
+  new_node = create_env_node(name);
+  while (current != NULL)
+  {
+    exp_node = (t_env *)current->content;
+    if (ft_strchr(exp_node->name, '='))
+    {
+      if ((ft_strncmp(exp_node->name, new_node->name, (ft_strlen(exp_node->name) -1)) == 0) && (ft_strlen(exp_node->name) - 1) == ft_strlen(new_node->name))
+      {
+        ft_printf("found\n");
+        if (previous == NULL)
+          *head = current->next;
+        else
+          previous->next = current->next;
+        ft_lstdelone(current, free);
+        return ;
+      }
+    }
+    previous = current;
+    current = current->next;
+  }
+}
+
 void ft_del_exp(t_list **head, char *name)
 {
   t_list  *current;
@@ -63,6 +94,6 @@ void  ft_unset(t_shell *myshell)
   else
   {
   ft_del_exp(&myshell->myexp, myshell->token[1]);
-  //ft_del_env(myshell);
+  ft_del_env(&myshell->myenv, myshell->token[1]);
   }
 }
